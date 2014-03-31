8d9dbe781313311cc868d5578c311558720abe33
jojo <showne@163.com>
2014-04-01 00:04:40 +0800

    尝试增加freemarker例子，页面显示，文件已经有了，但是没效果
    Signed-off-by: jojo <showne@163.com>

---
+++ work/jojo-web/src/main/java/com/jojo/webapp/Controller/IndexController.java
@@ -0,0 +1,37 @@
+/**
+ * JOJO
+ * 
+ * Copyright (c) 2013-2096 JOJO,Inc.All Rights Reserved.
+ */
+package com.jojo.webapp.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * <summary>
+ * 
+ * </summary>
+ * 
+ * @author jojo
+ * 
+ */
+@Controller
+public class IndexController
+{
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+    //, method = RequestMethod.GET
+    @RequestMapping(value = "/index")
+    public String test(HttpServletRequest request,HttpServletResponse response)
+    {
+        logger.info("进Action了");
+        return "index";  // 设置返回页面，这里对应 /WEB-INF/view 目录下的 index.ftl 文件
+    }
+}
