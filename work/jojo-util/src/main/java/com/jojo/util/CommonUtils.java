3ef82a4df1da7a84a4a925803ac67a0cd2d23557
jojo <showne@163.com>
2014-04-12 02:21:06 +0800

    maven 完整开发框架搭建完成，还需要调整具体的jar依赖。
    Signed-off-by: jojo <showne@163.com>

---
+++ work/jojo-util/src/main/java/com/jojo/util/CommonUtils.java
@@ -0,0 +1,19 @@
+/**
+ * JOJO
+ *
+ * Copyright (c) 2013-2096 JOJO,Inc.All Rights Reserved.
+ */
+package  com.jojo.util;
+
+/**
+ * <summary>
+ * 
+ * </summary>
+ * 
+ * @author jojo
+ * 
+ */
+public class CommonUtils
+{
+    public static final String version = "0.0.1";
+}
