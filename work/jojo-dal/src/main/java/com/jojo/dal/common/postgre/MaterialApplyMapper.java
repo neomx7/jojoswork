ff819946cca3e10081acda7e6951fc0ad4724f0d
jojo <showne@163.com>
2014-09-03 23:27:55 +0800

    申请表维护： 1.已经在数据库中建表 2.数据库文档补充 3.mapper类增加了增改删的功能， 还需实现 ..根据id单独查一条记录；翻页查列表的方法 ..页面保存 ..更新状态，随工作流节点变换。
    Signed-off-by: jojo <showne@163.com>

---
+++ work/jojo-dal/src/main/java/com/jojo/dal/common/postgre/MaterialApplyMapper.java
@@ -0,0 +1,77 @@
+/**
+ * JOJO
+ *
+ * Copyright (c) 2013-2096 JOJO,Inc.All Rights Reserved.
+ */
+package com.jojo.dal.common.postgre;
+
+import java.util.List;
+import java.util.Map;
+
+import com.jojo.dal.common.postgre.domain.MaterialApplyDO;
+
+/**
+ * <summary>
+ * <br>
+ * </summary>
+ *
+ * @author jojo
+ *
+ */
+public interface MaterialApplyMapper
+{
+
+    /**
+     *
+     * <summary>
+     * [[新增]]<br>
+     * <br>
+     * </summary>
+     *
+     * @author jojo
+     *
+     * @param applyDO
+     */
+    public void insert(MaterialApplyDO applyDO);
+
+    /**
+     *
+     * <summary>
+     * [更新]<br>
+     * <br>
+     * </summary>
+     *
+     * @author jojo
+     *
+     * @param params
+     */
+    public void update(Map<String, Object> params);
+
+    /**
+     *
+     * <summary>
+     * [删除]<br>
+     * <br>
+     * </summary>
+     *
+     * @author jojo
+     *
+     * @param params
+     */
+    public void delete(Map<String, Object> params);
+
+    /**
+     *
+     * <summary>
+     * [查询，翻页]<br>
+     * <br>
+     * </summary>
+     *
+     * @author jojo
+     *
+     * @param params
+     * @return
+     */
+    public List<MaterialApplyDO> queryPage(Map<String, Object> params);
+
+}
